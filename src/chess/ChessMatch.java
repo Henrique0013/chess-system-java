d6c9a38938214d9987eb95705624327d6d7b2328
Henrique0013 <rick_nils@hotmail.com>
2020-07-27 20:17:39 -0300

    Trocando jogador em cada turno

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -7,12 +7,24 @@ import chess.pieces.King;
 import chess.pieces.Rook;
 
 public class ChessMatch {
-
+	
+	private int turn;
+	private Color currentPlayer;
 	private Board board;
 
 	public ChessMatch() {
 		board = new Board(8, 8);
+		turn = 1;
+		currentPlayer = Color.WHITE;
 		initialSetup();
+	}
+	
+	public int getTurn() {
+		return turn;
+	}
+	
+	public Color getCurrentPlayer() {
+		return currentPlayer;
 	}
 
 	// Métodos
@@ -41,6 +53,7 @@ public class ChessMatch {
 		validateSourcePosition(source);
 		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target);
+		nextTurn();
 		return (ChessPiece) capturedPiece;
 	}
 
@@ -55,6 +68,11 @@ public class ChessMatch {
 		if (!board.thereIsAPiece(position)) {
 			throw new ChessException("There is no piece on source position");
 		}
+		
+		if(currentPlayer != ((ChessPiece)board.piece(position)).getColor()) {
+			throw new ChessException("The chosen piece is not yours");
+		}
+		
 		if (!board.piece(position).isThereAnyPossibleMove()) {
 			throw new ChessException("There is no possible moves for the chosen piece");
 		}
@@ -66,6 +84,16 @@ public class ChessMatch {
 		}
 
 	}
+	
+	/*
+	 * Expressão condicional ternária: Se o jogador atual for igual a Color.WHITE ? (então) agora
+	 * ele será Color.BLACK : (caso contrário) ele será Color.WHITE
+	 */	
+	private void nextTurn() {
+		turn++;
+		currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK: Color.WHITE;
+	}
+	
 
 	// o comando .toPosition converte o ChessPosition para a posição de matriz
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
