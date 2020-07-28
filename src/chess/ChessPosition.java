0f0e92c657c87e071b1a224e6728b1057767cf94
Henrique0013 <rick_nils@hotmail.com>
2020-07-28 19:59:42 -0300

    Logica do xeque

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -29,7 +29,7 @@ public class ChessPosition {
 	}
 	//... ((char)('a' - position.getColumn())... exemplo de casting
 	protected static ChessPosition fromPosition (Position position) {
-		return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
+		return new ChessPosition((char)('a' + position.getColumn()), 8 - position.getRow());
 	}
 	
 	@Override
